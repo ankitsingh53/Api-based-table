2c2519b7654fae7054189de2c3c0da8bdb4aaf25
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-04-28 17:35:52 +0530

    Data Fetched and shown in table

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-async function fetchData(){
+async function fetchData() {
     let response = await fetch('https://jsonplaceholder.typicode.com/users')
     let data = await response.json();
+    
+    console.log(data);
+
+
+    //  let row ='';
+    // data.forEach((element) => {
+    //     console.log(element);
+    //     row +=  `<tr>
+    //     <td>${element.id}</td>
+    //     <td>${element.name}</td>
+    //     <td>${element.username}</td>
+    //     </tr>
+    //     `
+    //     document.getElementById("table-body").innerHTML = row
+    // });
+
+    data.forEach((element) => {
+        
+        // console.log(element);
+
+        let row = document.createElement("tr")
+        row.innerHTML=  `
+        <td>${element.id}</td>
+        <td>${element.name}</td>
+        <td>${element.username}</td>
+        `
+        document.getElementById("table-body").append(row)
+    });
+    
 }
